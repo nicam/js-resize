9a6acc66ecefb47623ba5e4b78215af599665f8b
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2013-05-28 16:48:51 +0200

    added some docs

--- resize.js
+++ resize.js
@@ -1,14 +1,19 @@
 var Liip = Liip || {};
 Liip.resizer = (function ($) {
-    var cc;
+    var mainCanvas;
 
     var init = function () {
         $("#resize").click(startResize);
     };
 
+    /* 
+     * Creates a new image object from the src
+     * Uses the deferred pattern
+     */
     var createImage = function (src) {
         var deferred = $.Deferred();
         var img = new Image();
+
         img.onload = function() {
             deferred.resolve(img);
         };
@@ -16,32 +21,43 @@ Liip.resizer = (function ($) {
         return deferred.promise();
     };
 
+    /* 
+     * Create an Image, when loaded pass it on to the resizer
+     */
     var startResize = function () {
         $.when(
             createImage($("#inputImage").get(0).src)
         ).then(resize, function () {console.log('error')});
     };
 
+    /*
+     * Draw the image object on a new canvas and half the size of the canvas
+     * until the darget size has been reached
+     * Afterwards put the base64 data into the target image
+     */
     var resize = function (image) {
-        cc = document.createElement("canvas");
-        cc.width = 1024;
-        cc.height = 768;
-        var ctx = cc.getContext("2d");
-        ctx.drawImage(image, 0, 0, cc.width, cc.height);
+        mainCanvas = document.createElement("canvas");
+        mainCanvas.width = 1024;
+        mainCanvas.height = 768;
+        var ctx = mainCanvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, mainCanvas.width, mainCanvas.height);
         size = parseInt($('#size').get(0).value, 10);
-        while (cc.width > size) {
-            cc = halfSize(cc);
+        while (mainCanvas.width > size) {
+            mainCanvas = halfSize(mainCanvas);
         }
-        $('#outputImage').get(0).src = cc.toDataURL("image/jpeg");
+        $('#outputImage').get(0).src = mainCanvas.toDataURL("image/jpeg");
     };
 
+    /*
+     * Draw initial canvas on new canvas and half it's size
+     */
     var halfSize = function (i) {
-        var cc = document.createElement("canvas");
-        cc.width = i.width / 2;
-        cc.height = i.height / 2;
-        var ctx = cc.getContext("2d");
-        ctx.drawImage(i, 0, 0, cc.width, cc.height);
-        return cc;
+        var canvas = document.createElement("canvas");
+        canvas.width = i.width / 2;
+        canvas.height = i.height / 2;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(i, 0, 0, canvas.width, canvas.height);
+        return canvas;
     };
 
     return {
